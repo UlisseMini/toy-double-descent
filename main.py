af13bf3f2783e83a6fb36a2dd5a7e62d3855374c
Ulisse Mini <ulisse.mini@gmail.com>
2023-04-13 10:49:11 -0700

    CUDA support, save hparams to wandb

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import wandb
 
 wandb.init(project='toy-double-descent')
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # %%
 # Determinism
@@ -12,20 +13,21 @@ wandb.init(project='toy-double-descent')
 torch.manual_seed(0);
 
 # %%
-# Create dataset
+# Hyperparameters and dataset creation. (unkown = unknown in paper)
 
+config = wandb.config
+config.dim = 10_000    # N in paper
+config.pct = 0.999     # S in paper
+config.examples = 1000 # T in paper (unknown)
+config.hdim = int(config.dim * (1-config.pct)) # (unknown)
 
-dim = 10_000    # N in paper
-pct = 0.999     # S in paper
-examples = 1000 # T in paper (unknown)
+dim, pct, examples, hdim = config.dim, config.pct, config.examples, config.hdim
 
-# Create dataset of sparse vectors sampled from unif [0,1]
-
-
+# Create dataset of sparse vectors
 X = torch.rand(examples, dim)
 X[torch.rand(examples, dim) < pct] = 0
 X = X / torch.norm(X, dim=1, keepdim=True)
-
+X = X.to(device)
 
 # %%
 # Create model. essentially an autoencoder where W.T is the unembedding
@@ -33,8 +35,7 @@ X = X / torch.norm(X, dim=1, keepdim=True)
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        hidden_dim = int(dim * (1-pct)) # unknown
-        self.W = nn.Parameter(torch.randn(hidden_dim, dim))
+        self.W = nn.Parameter(torch.randn(hdim dim))
         self.b = nn.Parameter(torch.randn(dim))
 
         # Xavier initialization
@@ -53,6 +54,7 @@ def loss_fn(x, y):
 
 
 model = Model()
+model.to(device)
 
 print(f'num params = {sum(p.numel() for p in model.parameters()) / 1e3:.0f}k')
 print(f'loss = {loss_fn(model(X), X):.3f}')
@@ -61,7 +63,7 @@ print(f'loss = {loss_fn(model(X), X):.3f}')
 
 optim = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-for epoch in range(1000):
+for epoch in range(10000):
     optim.zero_grad()
     y = model(X)
     loss = loss_fn(y, X)
@@ -69,16 +71,21 @@ for epoch in range(1000):
     optim.step()
     
     if epoch % 10 == 0:
-        print(f'epoch {epoch} loss = {loss:.3f}')
+        print(f'epoch {epoch} loss = {loss}')
+        U, S, V = torch.svd(model.W)
         wandb.log({
             'loss': loss,
             'norm': torch.norm(model.W, dim=1).mean(),
             'std': model.W.std(),
+            'singular_values': wandb.Histogram(S.cpu().detach().numpy()),
         })
 
 # %%
 # Save model
 
 torch.save(model.state_dict(), 'model.pt')
-wandb.save('model.pt')
+artifact = wandb.Artifact('model', type='model')
+artifact.add_file('model.pt')
+wandb.run.log_artifact(artifact)
+
 
